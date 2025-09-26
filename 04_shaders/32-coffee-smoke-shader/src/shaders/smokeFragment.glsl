78745778e8b382736c8d3a493a8165c7bec36f78
Ravi-047 <ravi-47raj@gmail.com>
2025-09-26 08:05:23 +0530

    feat: add coffee smoke shader example with Three.js
    - Created package.json for project setup with Vite and dependencies.
    - Added README for setup instructions.
    - Implemented index.html for the main application structure.
    - Developed script.js to handle Three.js scene, camera, and rendering.
    - Introduced shader files for smoke effects (vertex and fragment shaders).
    - Added utility function for 2D rotation in shaders.
    - Styled the application with CSS for full-screen canvas.
    - Included static assets: baked model and perlin texture images.
    - Configured Vite for development and production builds.

---
+++ 04_shaders/32-coffee-smoke-shader/src/shaders/smokeFragment.glsl
@@ -0,0 +1,25 @@
+varying vec2 vUv;
+uniform sampler2D uPerlinTexture;
+uniform float uTime;
+
+void main() {
+    // scale and animate;
+    vec2 smokeUv = vUv;
+    smokeUv.x *= 0.5;
+    smokeUv.y -= 0.03 * uTime;
+
+    //smoke;
+    float smoke = texture2D(uPerlinTexture, smokeUv).r;
+    smoke = smoothstep(0.4, 1.0, smoke);
+
+    // edges
+    smoke *= smoothstep(0.0, 0.1, vUv.x);
+    smoke *= smoothstep(1.0, 0.9, vUv.x);
+    smoke *= smoothstep(0.0, 0.1, vUv.y);
+    smoke *= smoothstep(1.0, 0.4, vUv.y);
+
+    // output
+    gl_FragColor = vec4(0.6, 0.3, 0.2, smoke);
+    #include <tonemapping_fragment>
+    #include <colorspace_fragment>
+}
