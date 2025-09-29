985d699fc5e517185fc07b0c5d7cfaadf1cc114e
Ravi-047 <ravi-47raj@gmail.com>
2025-09-29 08:29:44 +0530

    Add Raging Sea Shading and Halftone Shading shaders
    - Created a new project for Raging Sea Shading with necessary files including package.json, index.html, and shaders.
    - Implemented water vertex and fragment shaders with customizable properties for wave elevation and colors.
    - Added ambient, directional, and point light calculations in GLSL for realistic lighting effects.
    - Set up a basic Three.js scene with a water mesh and controls.
    - Created a new project for Halftone Shading with similar structure and shaders.
    - Implemented halftone shading with customizable colors and added a GLTF model (Suzanne) to the scene.
    - Configured Vite for both projects to handle development and production builds.

---
+++ 04_shaders/36-raging-sea-shading-shaders/src/shaders/water/vertex.glsl
@@ -0,0 +1,59 @@
+uniform float uTime;
+uniform float uBigWavesElevation;
+uniform vec2 uBigWavesFrequency;
+uniform float uBigWavesSpeed;
+
+uniform float uSmallWavesElevation;
+uniform float uSmallWavesFrequency;
+uniform float uSmallWavesSpeed;
+uniform float uSmallIterations;
+
+varying float vElevation;
+varying vec3 vNormal;
+varying vec3 vPosition;
+
+#include '../includes/perlinClasic3D.glsl'
+
+float waveElevation(vec3 position) {
+    float elevation = sin(position.x * uBigWavesFrequency.x + uTime * uBigWavesSpeed) *
+        sin(position.z * uBigWavesFrequency.y + uTime * uBigWavesSpeed) *
+        uBigWavesElevation;
+
+    for(float i = 1.0; i <= uSmallIterations; i++) {
+        elevation -= abs(perlinClasic3D(vec3(position.xz * uSmallWavesFrequency * i, uTime * uSmallWavesSpeed)) * uSmallWavesElevation / i);
+    }
+    return elevation;
+}
+
+void main() {
+    // base position
+    float shift = 0.01;
+    vec4 modelPosition = modelMatrix * vec4(position, 1.0);
+    vec3 modelPositionA = modelPosition.xyz + vec3(shift, 0.0, 0.0);
+    vec3 modelPositionB = modelPosition.xyz + vec3(0.0, 0.0, -shift);
+
+    // Elevation
+    float elevation = waveElevation(modelPosition.xyz);
+    modelPosition.y += elevation;
+    modelPositionA.y += waveElevation(modelPositionA);
+    modelPositionB.y += waveElevation(modelPositionB); 
+
+    // compute normal
+    vec3 toA = normalize(modelPositionA - modelPosition.xyz);
+    vec3 toB = normalize(modelPositionB - modelPosition.xyz);
+    vec3 computeNormal = cross(toB, toA);
+
+    // Final position
+    vec4 viewPosition = viewMatrix * modelPosition;
+    vec4 projectedPosition = projectionMatrix * viewPosition;
+    gl_Position = projectedPosition;
+
+    // model normal 
+    // vec4 modelNormal = modelMatrix * vec4(normal, 0.0);
+    vec3 modelNormal = computeNormal;
+
+    // varying;
+    vElevation = elevation;
+    vNormal = modelNormal;
+    vPosition = modelPosition.xyz;
+}
