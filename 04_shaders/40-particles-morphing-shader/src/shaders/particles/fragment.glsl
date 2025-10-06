bfa111055011a2d4fcb9f81e8b9d51232c37d627
Ravi-047 <ravi-47raj@gmail.com>
2025-10-06 06:00:03 +0530

    Add initial shader model and Vite configuration for particles morphing shader
    - Added a new GLB model file for particle effects.
    - Created a Vite configuration file to set up the project with necessary plugins for GLSL and file watching.

---
+++ 04_shaders/40-particles-morphing-shader/src/shaders/particles/fragment.glsl
@@ -0,0 +1,12 @@
+varying vec3 vColor;
+
+void main() {
+    vec2 uv = gl_PointCoord;
+    float distanceToCenter = distance(uv, vec2(0.5));
+    float alpha = 0.05 / distanceToCenter - 0.1;
+
+    // gl_FragColor = vec4(vec3(1.0), alpha);
+    gl_FragColor = vec4(vColor, alpha);
+    #include <tonemapping_fragment>
+    #include <colorspace_fragment>
+}
