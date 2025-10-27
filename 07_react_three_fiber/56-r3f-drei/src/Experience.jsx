9fd277c7f46d00d717b4053dc6cc3e4d8d278851
Ravi-047 <ravi-47raj@gmail.com>
2025-10-27 11:41:45 +0530

    feat: initialize React Three Fiber project with Drei
    - Add package.json for project dependencies and scripts
    - Include custom font file for text rendering
    - Create Experience component with interactive 3D elements
    - Set up basic HTML structure in index.html
    - Implement entry point in index.jsx with Canvas and Experience
    - Add CSS styles for layout and label appearance
    - Configure Vite for development and build processes

---
+++ 07_react_three_fiber/56-r3f-drei/src/Experience.jsx
@@ -0,0 +1,74 @@
+import { MeshReflectorMaterial, Float, Text, Html, PivotControls, TransformControls, OrbitControls } from "@react-three/drei";
+import { useRef } from "react";
+
+
+export default function Experience() {
+    const cubeRef = useRef();
+    const sphereRef = useRef();
+
+    return <>
+        <OrbitControls makeDefault />
+        <directionalLight position={[1, 2, 3]} intensity={4.5} />
+        <ambientLight intensity={1.5} />
+
+        <PivotControls
+            anchor={[0, 0, 0]}
+            depthTest={false}
+            lineWidth={1}
+            axisColors={
+                ['#9381ff', '#ff4d6d', '#7ae582']
+            }
+            scale={1}
+            fixed={false}
+        >
+            <mesh ref={sphereRef} position-x={- 2}>
+                <sphereGeometry />
+                <meshStandardMaterial color="orange" />
+                <Html
+                    position={[1, 1, 0]}
+                    wrapperClass="label"
+                    center
+                    distanceFactor={6}
+                    occlude={[cubeRef, sphereRef]}
+                >
+                    Hello, I'm a HTML element
+                </Html>
+            </mesh>
+        </PivotControls>
+
+
+        <mesh ref={cubeRef} position-x={2} scale={1.5}>
+            <boxGeometry />
+            <meshStandardMaterial color="mediumpurple" />
+        </mesh>
+        <TransformControls object={cubeRef} mode="translate" />
+
+        <mesh position-y={- 1} rotation-x={- Math.PI * 0.5} scale={10}>
+            <planeGeometry />
+            {/* <meshStandardMaterial color="greenyellow" /> */}
+            <MeshReflectorMaterial
+                resolution={512}
+                blur={[1000, 1000]}
+                mixBlur={1}
+                mirror={0.75}
+                color="greenyellow"
+            />
+        </mesh>
+
+        <Float
+            speed={5}
+        >
+            <Text
+                font="/bangers-v20-latin-regular.woff"
+                color='salmon'
+                fontSize={1}
+                position-y={2}
+                maxWidth={2}
+                textAlign="center"
+            >
+                I Love R3F
+                {/* <meshNormalMaterial /> */}
+            </Text>
+        </Float>
+    </>
+}
